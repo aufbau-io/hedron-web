ed1489dcfa89f84f19bb43c4e74f078a55a65d2f
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-10-06 15:37:24 -0700

    gallery

--- src/routes/(page)/gallery/+page.svelte
+++ src/routes/(page)/gallery/+page.svelte
@@ -3,7 +3,19 @@
 
 <main>
 	<span class="justify-letters">
-		<p class="lrg">gallery</p>
+		<div class="scroll">
+			<img class="slide" src="/assets/4.jpg" alt="img" />
+			<img class="slide" src="/assets/11.jpg" alt="img" />
+			<img class="slide" src="/assets/2.jpg" alt="img" />
+			<img class="slide" src="/assets/3.jpg" alt="img" />
+			<img class="slide" src="/assets/5.jpg" alt="img" />
+			<img class="slide" src="/assets/6.jpg" alt="img" />
+			<img class="slide" src="/assets/7.jpg" alt="img" />
+			<img class="slide" src="/assets/8.jpg" alt="img" />
+			<img class="slide" src="/assets/9.jpg" alt="img" />
+			<img class="slide" src="/assets/10.jpg" alt="img" />
+			<img class="slide" src="/assets/1.jpg" alt="img" />
+		</div>
 	</span>
 </main>
 
@@ -14,12 +26,12 @@
 		left: 0;
 		transform: translateY(-50%);
 
-		background: var(--black);
 		border-top: solid 1px var(--accent);
 		border-bottom: solid 1px var(--accent);
 
 		width: 100vw;
-		padding: 10px 40px;
+		height: 100%;
+		padding: 0 0;
 
 		color: var(--accent);
 		text-transform: uppercase;
@@ -31,17 +43,43 @@
 		font-family: 'nb-architekt', sans-serif;
 	}
 
+	.scroll {
+		width: 100%;
+		height: 100%;
+		padding-top: 50px;
+		padding-bottom: 100px;
+		border: solid 1px var(--accent);
+		overflow-y: auto;
+	}
+
+	.scroll img {
+		width: inherit;
+	}
+
 	.justify-letters {
 		display: inline-block;
 		text-align: justify;
 		text-align-last: justify;
 		width: 66.7%;
+		height: 100%;
 		margin: 0 auto;
+		overflow: auto;
+	}
+
+	@media only screen and (max-width: 1080px) {
+		.scroll {
+			padding-bottom: 120px;
+		}
 	}
 
 	@media only screen and (max-width: 768px) {
 		.justify-letters {
 			width: 100%;
+		}
+
+		.scroll {
+			padding-top: 70px;
+			padding-bottom: 220px;
 		}
 	}
 </style>
