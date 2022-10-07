34bf7c7121393e2febcee31111eafd8dd437692f
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-10-07 14:03:49 -0700

    update discord link

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -12,7 +12,7 @@
 	{#if showFooter == true || $screenType == 3}
 		<section>
 			<div class="base highlight">
-				<a href="https://discord.gg/ZSknb6pw5c" class="discord"><h5>Join us on Discord</h5></a>
+				<a href="https://discord.gg/X84tscW" class="discord"><h5>Join us on Discord</h5></a>
 				<a href="https://hhacker.space/wiki/" class="wiki"><h5>Visit our Wiki</h5></a>
 			</div>
 			<div class="base">
