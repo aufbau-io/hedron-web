3d9a19ba4481438189fb1d819eddf848de181c80
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-12-13 15:59:28 +0000

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,16 +8,23 @@
 		// SCREEN
 		// ---------------------------------------------------------------------------
 		const ua = navigator.userAgent;
-		if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-			// tablet
+		if (window.location !== window.parent.location) {
+			// The page is in an iframe
+			// default to phone on iframe
 			screenType.set(1);
+		} else if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+			// The page is not in an iframe
+			// tablet
+			screenType.set(2);
 		} else if (
 			/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
 				ua
 			)
 		) {
 			// phone
-			screenType.set(2);
+			// darkMode.set(false);
+			// document.querySelector(':root').classList.toggle('dark-mode');
+			screenType.set(1);
 		} else {
 			//laptop
 			screenType.set(3);
